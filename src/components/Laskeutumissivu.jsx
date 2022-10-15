d4962193c747dba6649a4223dced0e78a44574d6
Jusener <59564904+justusBerner@users.noreply.github.com>
2022-10-16 00:17:12 +0300

    CSS1

--- src/components/Laskeutumissivu.jsx
+++ src/components/Laskeutumissivu.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 export default function Laskeutumissivu(props) {
+  const vartio = props.vartioData
   return (
     <div>
-      <h1>{props.vartioData.vartio}</h1>
-      <button onClick={() => (window.location.href = `/kirje/${props.vartioData.tilaus}`)}>
+      
+      <nav className='navi'>
+        <li><a href="#">Etusivu</a></li>
+        <li><a href="#">Tietoa kisasta</a></li>
+        <li><a href="#">Seuraa somessa!</a></li>
+      </nav>
+      <h1>Vartion nimi: {vartio.vartio}</h1>
+      <h2>{vartio.lpk}</h2>
+      <h2>{vartio.vartionro}</h2>
+      <button onClick={() => (window.location.href = `/kirje/${vartio.tilaus}`)}>
         Lataa kirje
       </button>
     </div>
