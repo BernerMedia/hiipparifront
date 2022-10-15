f53a0a0533fc83e1a9f98db0a229221dffcb591c
Ode42 <ode.progr@outlook.com>
2022-10-15 14:20:23 +0300

    Reititys

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import HiippariRouter from './Router';
+
+function App() {
+  return (
+    <div className="App">
+      <HiippariRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Laskeutumissivu.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Laskeutumissivu() {
+  return <div>Laskeutumissivu</div>;
+}
