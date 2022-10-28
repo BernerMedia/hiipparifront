f11efd51e9f2066b4fb4a54f1feec6694a820580
Jusener <59564904+justusBerner@users.noreply.github.com>
2022-10-28 20:46:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,8 @@ import HiippariRouter from './HiippariRouter';
 
 function App() {
   return (
+    
     <div className="App">
-    <div className="hello">    <h1>Hei kisaaja!</h1>
-      <p>Vartionjohtaja on saanut sähköpostiinsa vartionne oman linkin, jolla pääsette lukemaan kisakirjeen ja teidän vartiokohtaiset tiedot</p>
-
-      <button id='etusivub'> <a target={'_blank'} href="https://hh.ilvesveikot.fi/">Hiipparin etusivulle</a></button>
-     </div>
    <HiippariRouter />
     </div>
   );
