c533fe34c0a8fde4cd173fd41ea31c6ed5bef33a
Jusener <59564904+justusBerner@users.noreply.github.com>
2022-10-28 20:22:58 +0300

    etusivulle tekstin ja otsikon lisäys

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import HiippariRouter from './HiippariRouter';
 function App() {
   return (
     <div className="App">
-      <HiippariRouter />
+    <div className="hello">    <h1>Hei kisaaja!</h1>
+      <p>Vartionjohtaja on saanut sähköpostiinsa vartionne oman linkin, jolla pääsette lukemaan kisakirjeen ja teidän vartiokohtaiset tiedot</p>
+
+      <button id='etusivub'> <a target={'_blank'} href="https://hh.ilvesveikot.fi/">Hiipparin etusivulle</a></button>
+     </div>
+   <HiippariRouter />
     </div>
   );
 }
